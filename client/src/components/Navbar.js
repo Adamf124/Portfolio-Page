351b4b5a7930ad6354a9302c536d66eef0440ab4
Adam <adam.ferguson124@gmail.com>
2020-03-07 09:41:38 -0500

    Add NavBar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-end">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarNavAltMarkup" 
+                    aria-controls="navbarNavAltMarkup" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#">About</a>
+                            <a className="nav-item nav-link" href="#">Projects</a>
+                            <a className="nav-item nav-link" href="#">Social</a>
+                            <a className="nav-item nav-link" href="#">Contact Me</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
--- client/src/components/Splashpage.js
+++ client/src/components/Splashpage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
 
 export default class Splashpage extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="App">
+            <header className="App-header">
+                <Navbar/>
+              </header>
             </div>
         )
     }
