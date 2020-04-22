fa5a4f561b2c8f07e937c8bc0828d6908aec13aa
Adam <adam.ferguson124@gmail.com>
2020-04-21 21:07:28 -0400

    welcome skeleton

--- client/src/components/Splashpage.js
+++ client/src/components/Splashpage.js
@@ -13,9 +13,16 @@ export default class Splashpage extends Component {
                 <div className='splash'>
                     <div className="welcome-mat">
                         <h1 className="welcome-text welcome-text-sm d-xs-block d-lg-none">WELCOME</h1>
-                        <h1 className="welcome-text d-none d-lg-block">WELCOME LARGE</h1>
+                        <h1 className="welcome-text d-none d-lg-block">WELCOME LARGE </h1>
                     </div>
                 </div>
+                <div id="about"className='container'>
+                    About
+                </div>
+                <div id="projects"className='container'>Projects</div>
+                <div id="social"className='container'>Social</div>
+                <div id="contact-me"className='container'>Contact  Me</div>
+
             </div>
         )
     }
